257dee7ca856e8ca802a6d2659dae6a479d1cc3f
yuntse <yuntse@slac.stanford.edu>
2016-04-27 12:42:39 -0500

    Tighten error checking.

--- scripts/project.py
+++ scripts/project.py
@@ -1267,11 +1267,14 @@ def docheck(project, stage, ana):
         print '%d unconsumed files.' % nerror
 
     # Return error status if any error or not good root file produced.
+    # Also return error if no successful processes were detected
 
     result = 0
     if nerror != 0:
         result = 1
     if not ana and nroot_tot == 0:
+        result = 1
+    if len(procmap) == 0:
         result = 1
     return result
 
