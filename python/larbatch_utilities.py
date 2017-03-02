2b270f2029d493644197f78c2020d006c8536716
Herbert Greenlee <greenlee@fnal.gov>
2017-03-02 15:41:35 -0600

    Increase timeout for ifdh_cp to 600s.

--- python/larbatch_utilities.py
+++ python/larbatch_utilities.py
@@ -102,7 +102,7 @@ def ifdh_cp(source, destination):
     q = Queue.Queue()
     thread = threading.Thread(target=wait_for_subprocess, args=[jobinfo, q])
     thread.start()
-    thread.join(timeout=60)
+    thread.join(timeout=600)
     if thread.is_alive():
         print 'Terminating subprocess.'
         jobinfo.terminate()
